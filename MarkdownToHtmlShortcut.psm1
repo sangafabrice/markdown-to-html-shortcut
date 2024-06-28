8505ca68fe88c5652d9d69dde9c19e8aa8b46a1d
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-06-28 18:43:13 +0100

    build: Remove alias from module script

--- MarkdownToHtmlShortcut.psm1
+++ MarkdownToHtmlShortcut.psm1
@@ -52,8 +52,6 @@ Function Set-MarkdownToHtmlShortcut {
   Set-ItemProperty -Path $CommandKey.PSParentPath -Name 'Icon' -Value "$PSScriptRoot\shortcut-icon.ico" -Force
 }
 
-Set-Alias Install-MarkdownToHtmlShortcut Set-MarkdownToHtmlShortcut
-
 Function Remove-MarkdownToHtmlShortcut {
   <#
   .SYNOPSIS
