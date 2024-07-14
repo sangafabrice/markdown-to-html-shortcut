7123c0326e78946d6763416063882485b7f00dc6
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-07-14 11:38:02 +0100

    docs: Reorganize the assemblies using conditional compilation

--- Convert-MarkdownToHtml.cs
+++ Convert-MarkdownToHtml.cs
@@ -1,36 +1,61 @@
 ﻿using System;
-using System.IO;
-using System.Diagnostics;
 using System.Reflection;
+#if DLL_LIBRARY
+using System.Diagnostics;
+#else
+using System.IO;
+using MarkdownToHtml.Shortcut;
+#endif
 
-[assembly: AssemblyTitle("Convert Markdown to HTML Launcher")]
 [assembly: AssemblyProduct("MarkdownToHtml Shortcut")]
 [assembly: AssemblyInformationalVersion("0.4.0.0")]
 [assembly: AssemblyCopyright("© 2024 sangafabrice")]
 [assembly: AssemblyCompany("sangafabrice")]
 [assembly: AssemblyVersion("0.4.0.0")]
 
+// DLL_LIBRARY conditional compilation symbol for
+// specifying that the assembly is the library.
+#if DLL_LIBRARY
+[assembly: AssemblyTitle("MarkdownToHtml Shortcut Launcher Library")]
+
+namespace MarkdownToHtml.Shortcut
+{  
+  /**
+   * Represents the launcher of the shortcut link.
+  */
+  public class Launcher
+  {
+    /// <summary>Launch a hidden Command Prompt that runs the shortcut link.</summary>
+    /// <param name="ShortcutPath">The shortcut link path to target shortcut menu script.</param>
+    /// <param name="MarkdownPath">The markdown path input as argument to the shortcut link.</param>
+    public static void Start(string ShortcutPath, string MarkdownPath)
+    {
+      var PwshStartInfo = new ProcessStartInfo(
+        "cmd.exe",
+        String.Format("/d /c \"\"\"{0}\"\" \"\"{1}\"\"\"",ShortcutPath,MarkdownPath)
+      );
+      // HIDE_CONSOLE conditional compilation symbol for
+      // specifying that the window style should be Hidden.
+      #if HIDE_CONSOLE
+      PwshStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      #endif
+      Process.Start(PwshStartInfo);
+    }
+  }
+}
+#else
+[assembly: AssemblyTitle("Convert Markdown to HTML Launcher")]
+
 class ConvertMarkdownToHtml
 {
-  /// <summary>Launch a hidden Command Prompt that runs the shortcut link.</summary>
+  /// <summary>Call the Launcher.Start method.</summary>
   /// <param name="args"> The command line arguments.</param>
   static void Main(string[] args)
   {
-    var PwshStartInfo = new ProcessStartInfo(
-      "cmd.exe",
-      String.Format(
-        "/d /c \"\"\"{0}\"\" \"\"{1}\"\"\"",
-        // The link path the same as the process path except the extension.
-        Path.ChangeExtension((Environment.GetCommandLineArgs())[0],".lnk"),
-        // The input markdown file path passed as argument to the link.
-        args[0]
-      )
+    Launcher.Start(
+      Path.ChangeExtension((Environment.GetCommandLineArgs())[0],".lnk"),
+      args[0]
     );
-    // HIDE_CONSOLE conditional compilation symbol for
-    // specifying that the window style should be Hidden.
-    #if HIDE_CONSOLE
-    PwshStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-    #endif
-    Process.Start(PwshStartInfo);
   }
 }
+#endif
