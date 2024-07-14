72c4119ccbf83a66213f16510877ab816273e5f8
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-07-14 13:22:25 +0100

    docs: Reorganize the assemblies using conditional compilation

--- Convert-MarkdownToHtml.vb
+++ Convert-MarkdownToHtml.vb
@@ -1,34 +1,55 @@
 ﻿Option Explicit On
 
 Imports System
-Imports System.IO
-Imports System.Diagnostics
 Imports System.Reflection
+#If DLL_LIBRARY Then
+Imports System.Diagnostics
+#Else
+Imports System.IO
+Imports MarkdownToHtml.Shortcut
+#End If
 
-<Assembly: AssemblyTitle("Convert Markdown to HTML Launcher")>
 <Assembly: AssemblyProduct("MarkdownToHtml Shortcut")>
 <Assembly: AssemblyInformationalVersion("0.4.0.0")>
 <Assembly: AssemblyCopyright("© 2024 sangafabrice")>
 <Assembly: AssemblyCompany("sangafabrice")>
 <Assembly: AssemblyVersion("0.4.0.0")>
 
-Module MarkdownToHtmlShortcut
-  ''' <summary>Launch a hidden Command Prompt that runs the shortcut link.</summary>
+' DLL_LIBRARY conditional compilation symbol for
+' specifying that the assembly is the library.
+#If DLL_LIBRARY Then
+<Assembly: AssemblyTitle("MarkdownToHtml Shortcut Launcher Library")>
+
+Namespace MarkdownToHtml.Shortcut
+  Public Class Launcher
+    ''' <summary>Launch a hidden Command Prompt that runs the shortcut link.</summary>
+    ''' <param name="ShortcutPath">The shortcut link path to target shortcut menu script.</param>
+    ''' <param name="MarkdownPath">The markdown path input as argument to the shortcut link.</param>
+    Public Shared Sub Start(ByVal ShortcutPath As String, ByVal MarkdownPath As String)
+      Dim PwshStartInfo As New ProcessStartInfo(
+        "cmd.exe",
+        String.Format("/d /c """"""{0}"""" """"{1}""""""",ShortcutPath,MarkdownPath)
+      )
+      ' HIDE_CONSOLE conditional compilation symbol for
+      ' specifying that the window style should be Hidden.
+      #If HIDE_CONSOLE Then
+      PwshStartInfo.WindowStyle = ProcessWindowStyle.Hidden
+      #End if
+      Process.Start(PwshStartInfo)
+    End Sub
+  End Class
+End Namespace
+#Else
+<Assembly: AssemblyTitle("Convert Markdown to HTML Launcher")>
+
+Module ConvertMarkdownToHtml
+  ''' <summary>Call the Launcher.Start method.</summary>
   ''' <param name="args"> The command line arguments.</param>
   Sub Main(ByVal args() As String)
-    ' The input markdown file path passed as argument to the link.
-    Dim MarkdownPath As String = args(0)
-    ' The link path the same as the process path except the extension.
-    Dim LinkPath As String = Path.ChangeExtension((Environment.GetCommandLineArgs())(0),".lnk")
-    Dim PwshStartInfo As New ProcessStartInfo(
-      "cmd.exe",
-      String.Format("/d /c """"""{0}"""" """"{1}""""""",LinkPath,MarkdownPath)
+    Launcher.Start(
+      Path.ChangeExtension((Environment.GetCommandLineArgs())(0),".lnk"),
+      args(0)
     )
-    ' HIDE_CONSOLE conditional compilation symbol for
-    ' specifying that the window style should be Hidden.
-    #If HIDE_CONSOLE Then
-    PwshStartInfo.WindowStyle = ProcessWindowStyle.Hidden
-    #End if
-    Process.Start(PwshStartInfo)
   End Sub
 End Module
+#End If
