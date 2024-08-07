2f00f6a7b22db41b367ed6bd1a687bdc42a4143b
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-08-07 22:31:51 +0100

    refactor: Create dynamic shortcut link

--- MarkdownToHtml.Shortcut.psm1
+++ MarkdownToHtml.Shortcut.psm1
@@ -21,27 +21,6 @@ Function Set-MarkdownToHtmlShortcut {
   Function Private:Set-ConvertMd2HtmlExtension([string] $Extension) {
     Return "$PSScriptRoot\Convert-MarkdownToHtml$Extension"
   }
-  # Store the shortcut link path string which base name is the name as the root module.
-  $ShortcutLinkIcon = [Path]::ChangeExtension($PSCommandPath, '.ico')
-  # Store the WScript.EXE full path string.
-  $WscriptExe = 'c:\windows\system32\wscript.exe'
-  # Store the path to the launcher script.
-  $LauncherScript = Set-ConvertMd2HtmlExtension '.js'
-  # Create the shortcut link to WSH Wscript application.
-  (New-Object -ComObject WScript.Shell).CreateShortcut((Set-ConvertMd2HtmlExtension '.lnk')) |
-  ForEach-Object {
-    $_.TargetPath = $WscriptExe
-    # The command is partial because it does not include the markdown file path string.
-    # The markdown file path string will be input when calling the shortcut link.
-    $_.Arguments = '//E:jscript "{0}"' -f $LauncherScript
-    $_.IconLocation = $ShortcutLinkIcon
-    $_.Description = 'Launch a WScript process to run the shortcut link that will set the custom icon.'
-    $_.Save()
-    $ShortcutLinkPath = $_.FullName
-    If (-not (Test-Path $ShortcutLinkPath -PathType Leaf)) {
-      Throw [FileNotFoundException]::New(('Cannot find path "{0}" because it does not exist.' -f $ShortcutLinkPath), $ShortcutLinkPath)
-    }
-  }
   # The arguments to Set-Item and New-Item cmdlets.
   $Arguments = @{
     # The registry key of the command executed by the shortcut.
@@ -49,7 +28,7 @@ Function Set-MarkdownToHtmlShortcut {
     # %1 is the path to the selected mardown file to convert.
     # The script to hide the PowerShell console window is executed in GUI mode (WScript).
     # I keep using WScript.EXE because I cannot use the link directly in the registry.
-    Value = '{1} //E:jscript "{0}" /MarkdownPath:"%1" /RunLink' -f $LauncherScript,$WscriptExe
+    Value = 'C:\Windows\System32\wscript.exe //E:jscript "{0}" /MarkdownPath:"%1" /RunLink' -f (Set-ConvertMd2HtmlExtension '.js')
   }
   # Overwrite the key value if it already exists.
   # Otherwise, create it.
@@ -69,7 +48,7 @@ Function Set-MarkdownToHtmlShortcut {
     Remove-ItemProperty @Arguments -ErrorAction SilentlyContinue
     Return
   }
-  Set-ItemProperty @Arguments -Value $ShortcutLinkIcon
+  Set-ItemProperty @Arguments -Value (Set-ConvertMd2HtmlExtension '.ico')
 }
 
 Function Remove-MarkdownToHtmlShortcut {
